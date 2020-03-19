2a5233ba95aac060f8c9155473c499e4fe91a99f
Deck Sendy <57905499+sendydwipa@users.noreply.github.com>
2020-03-19 18:22:03 +0700

    video electron part 5
    pemrograman visual

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ ipcMain.on("appointment:request:list", event =>{
     console.log("here");
 });
 
+ipcMain.on("appointment:request:today", event =>{
+    console.log("here2");
+})
+
+ipcMain.on("appointment:done", (event, id) =>{
+    console.log("here3")
+})
+
 const menuTemplate = [
     {
         label: "file",
